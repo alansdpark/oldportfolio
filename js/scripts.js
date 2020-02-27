40d063791fce47d9003d51486e1372f4ce8c1627
Alan Park <alansdpark@gmail.com>
2020-02-26 16:32:18 -0800

    old site

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+	var isArrowDown = true;
+
+	$(".portfolioTopButton").click(function(event) {
+		event.preventDefault();
+
+		if (isArrowDown) { // expand the portfolio automatically and scroll to it, else just do the rest.
+			$("#arrowDown").click();
+		}
+
+		$("html, body").animate({ scrollTop: $("#portfolioTop").offset().top}, 800);
+		return false;
+	});
+
+	$(".contactTopButton").click(function(event) { // Scrolls to contact section
+		event.preventDefault();
+
+		if (isArrowDown) {
+			$("#arrowDown").click();
+		}
+
+		$("html, body").animate({ scrollTop: $("#ContactSection").offset().top}, 800);
+		return false;
+	});
+
+	$("#arrowDown").click(function(event) { // Change icons based on whether drawer is open or not.
+		if (isArrowDown) {
+			isArrowDown = false;
+
+		} else {
+			isArrowDown = true;
+		}
+		$("#changeArrow").toggleClass('fa-angle-down').toggleClass('fa-angle-up');
+
+	});
+});
